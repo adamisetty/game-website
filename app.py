f6d9a55f483484c9f7aa733daba7c418ad574a53
ankithadamisetty <ankithad@illinois.edu>
2020-07-05 19:58:35 -0500

    adds basic endpoint in Flask

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask('game_website')
+
+@app.route('/')
+def home():
+    return "Welcome!"
+
+@app.route('/post', methods=['GET'])
+def post():
+    return "We should return some sort of JSON here"
+
+
+if __name__ == '__main__':
+    app.run()
+    #app.run(debug=True)
