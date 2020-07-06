a75d454246044398cad451704e2334011b096249
ankithadamisetty <ankithad@illinois.edu>
2020-07-06 17:40:16 -0500

    adds basic post and get functions in api.py

---
+++ api.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+#from games.tictactoe import make_board
+from games import tictactoe as tt
+
+app = Flask('game_website')
+responses = []
+
+@app.route('/')
+def home():
+    return "Welcome!"
+
+@app.route('/post', methods=['POST'])
+def test():
+    response = {
+        'board' : tt.make_board()
+    }
+    responses.append(response)
+    print('Called post')
+    return jsonify({'response' : response}), 201
+
+
+@app.route('/test', methods=['GET'])
+def get():
+    print('called Get')
+    return jsonify({'responses': responses})
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(debug=True)
---
+++ games/tictactoe.py
@@ -0,0 +1,3 @@
+
+def make_board():
+    return '_________'
