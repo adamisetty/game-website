176c5a32d15de60e8bb15e7d36ed0c7d2efb09a2
alyciab2 <alyciab2@illinois.edu>
2020-07-10 17:56:03 -0500

    made api.py up to date with frontend branch

--- api.py
+++ api.py
@@ -3,26 +3,34 @@ from flask import Flask, jsonify, request
 from games import tictactoe as tt
 
 app = Flask('game_website')
-responses = []
+games_data = []
 
 @app.route('/')
 def home():
     return "Welcome!"
 
-@app.route('/post', methods=['POST'])
-def test():
+@app.route('/<game>/create', methods=['POST'])
+#add parameter for which game later
+def create_game():
     response = {
-        'board' : tt.make_board()
+        'game' : request.args.get('name'),
+        'board' : tt.make_board(),
+        'isWinner' : False,
+        'winner' : None,
+        'score' : 0
     }
-    responses.append(response)
-    print('Called post')
-    return jsonify({'response' : response}), 201
+    games_data.append(response)
+    return jsonify({'games_data' : games_data}), 201
 
 
-@app.route('/test', methods=['GET'])
+@app.route('/<game>/get-board', methods=['GET'])
 def get():
-    #print('called Get')
-    return jsonify({'responses': responses})
+    return jsonify({'games_data': games_data})
+
+@app.rout('/<game>/make-turn', methods=['PUT'])
+def turn():
+    games_data[0]['board'] = tt.make_move()
+    return jsonify({'games_data' : games_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
