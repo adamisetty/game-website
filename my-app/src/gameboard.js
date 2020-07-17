d6c29c1768a478e427cccbcc3f1d92b8b88515c2
shalnisundram <shalnis2@illinois.edu>
2020-07-17 14:14:05 -0700

    create gameboard.html and gameboard.css to display board

--- my-app/src/gameboard.js
+++ my-app/src/gameboard.js
@@ -1,50 +1,71 @@
 import React from 'react';
+
 import './gameboard.css';
 
-function Tile(props) {
-    return (
-    <button className="tile" onClick={props.onClick}>
-        {props.value}
-    </button>   
-    ); 
-}
-
-class TicTacToeBoard extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            tiles: Array(9).fill(null)
-        };
-    }
-
-    renderTile(i) {
-        return (
-            <Tile 
-                value = {this.state.tiles[i]}
-            />    
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                </div>
-                <div className="board-row">
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                </div>
-                <div className="board-row">
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                 {this.renderTile(0)}
-                </div>
-            </div>
-        )
-    }
-}
+//function Game() {
+//return
+//<div id="errors" style="
+//  background: #c00;
+//  color: #fff;
+//  display: none;
+//  margin: -20px -20px 20px;
+//  padding: 20px;
+//  white-space: pre-wrap;
+//"></div>
+//<div id="root"></div>
+//}
+//
+//function Tile(props) {
+//    return (
+//    <button className="tile" onClick={props.onClick}>
+//        {props.value}
+//    </button>
+//    );
+//}
+//
+//class TicTacToeBoard extends React.Component {
+//
+//    constructor(props) {
+//        super(props);
+//        this.state = {
+//            tiles: Array(9).fill(null)
+//        };
+//    }
+//
+//    renderTile(i) {
+//        return (
+//            <Tile
+//                value = {this.state.tiles[i]}
+//            />
+//        )
+//    }
+//
+//    render() {
+//        return (
+//            <div>
+//                <div className="board-row">
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                </div>
+//                <div className="board-row">
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                </div>
+//                <div className="board-row">
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                 {this.renderTile(0)}
+//                </div>
+//            </div>
+//        );
+//    }
+//}
+//
+//ReactDOM.render(
+//  <TicTacToeBoard />,
+//  document.getElementById('root')
+//);
+//
+//export default Tile;
