2347565c14beb924b25c3a6e2c5f87cef6b64b25
alyciab2 <alyciab2@illinois.edu>
2020-07-29 19:33:48 -0500

    added bullandcows python file & generates random secret code

---
+++ games/bullsandcows.py
@@ -0,0 +1,64 @@
+import time
+import random
+import math
+
+class BullsAndCows :
+
+
+    def __init__(self):
+        self.secret_code = self.generate_secret_code()
+        self.SECRETCODELEN = 4
+        self.board = []
+        self.winner = None 
+        self.isWinner = False
+        self.current_player = None
+        self.initial_time = time.time()
+        self.final_time = time.time()
+        self.score = initial_time - initial_time
+
+
+    def generate_secret_code(self) :
+        code = ""
+        num = ""
+        for i in range(SECRETCODELEN) :
+            while (str(num) in code) :
+                num = random.randint(0,9)
+            code = code + str(num)
+        return code
+
+
+    def has_duplicates(self, string) :
+
+
+    def place_mark(self, guess) :
+        turn_list = []
+        if len(guess) != self.SECRETCODELEN :
+            turn_list.append("Invalid Guess")
+        else if has_duplicates(guess) :
+            turn_list.append("Duplicates Present")
+    
+    def get_winner(self) :
+        return self.winner
+
+    
+    def is_game_over(self) :
+        return self.isWinner
+
+
+    def get_current_player(self) :
+        return self.current_player
+
+    
+    def get_score(self) :
+        return math.floor(self.final_time - self.initial_time)
+
+
+    def reset(self) :
+        self.secret_code = self.generate_secret_code()
+        self.SECRETCODELEN = 4
+        self.board = []
+        self.winner = None 
+        self.isWinner = False
+        self.current_player = None
+        self.initial_time = time.time()
+        self.score = initial_time - initial_time
