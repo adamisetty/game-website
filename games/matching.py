eb9c513c9432f9052a71d8bac80349671e2fa1ff
shalnisundram <shalnis2@illinois.edu>
2020-08-01 02:48:01 -0700

    include matching into api.py and engine.py

--- games/matching.py
+++ games/matching.py
@@ -1,66 +1,79 @@
 import random
-import time
-
 
 class Matching:
-
     def __init__(self):
-        self.game_over = [False]
+        self.game_over = False
+        self.current_player = None
         self.current_matches = 0
-        self.current_player = []
-        self.hidden_board = ['Blimey!', 'Blimey!', 'Rubbish!', 'Rubbish!', 'Minging!' 'Minging!', 'Cheerio!',
+        self.strikes = 0
+        self.old_positions = []
+        self.chosen_positions = []
+        self.first_pos = 0
+        self.second_pos = 0
+        self.original_board = ['Blimey!', 'Blimey!', 'Rubbish!', 'Rubbish!', 'Minging!', 'Minging!', 'Cheerio!',
                              'Cheerio!',
                              'Codswallop! ', 'Codswallop!', 'Gutted', 'Gutted', 'Bobs Your Uncle!', 'Bobs Your Uncle!',
                              'Brilliant!', 'Brilliant!']  # 16 spaces
-        self.blank_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.blank_board = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
         self.TOTAL_MATCHES = 8
+        self.hidden_board = random.sample(self.original_board, len(self.original_board)) # Shuffles words for new game
+        print(self.hidden_board)
+
+    # Makes a turn, first_pos and second_pos are player's first and second guesses
+    def place_mark(self, positions):
+        self.check_game_over()
+        chosen_positions = positions.split("-")
+        self.first_pos = int(chosen_positions[0])
+        self.second_pos = int(chosen_positions[1])
+
+        # Checks if words have already been matched
+        for i in range(len(self.old_positions)):
+            if self.old_positions[i] == self.first_pos or self.old_positions[i] == self.second_pos:
+                return self.blank_board
+
+        # Checks if the chosen words are the same
+        if self.hidden_board[self.first_pos] == self.hidden_board[self.second_pos]:
+            self.current_matches += 1
+            self.blank_board[self.first_pos] = 'X'  # Marks square as a found match
+            self.blank_board[self.second_pos] = 'X'
+            self.old_positions.append(self.first_pos)
+            self.old_positions.append(self.second_pos)
+        else:
+            self.strikes += 1
+            self.blank_board[self.first_pos] = "_"  # Hides the word
+            self.blank_board[self.second_pos] = "_"
+        return self.blank_board
+
+    def check_game_over(self):
+        if self.current_matches == self.TOTAL_MATCHES:
+            self.game_over = True
 
     def is_game_over(self):
-        return "hello"
-
-    def check_same_word(self, first_pos, second_pos):
-        if self.hidden_board[first_pos] == self.hidden_board[second_pos]:
-            self.current_matches += 1
-            self.blank_board[first_pos] = 'X'  # Marks square as a found match
-            self.blank_board[second_pos] = 'X'
-        else:
-            time.sleep(2)  # Gives player 2 seconds to remember words before hidden again
-            self.blank_board[first_pos] = ""  # Hides the word
-            self.blank_board[second_pos] = ""
+        return self.game_over
 
     def get_winner(self):
-        if self.game_over:
-            return "You won!"
+        return self.strikes
 
     def get_current_player(self):
-        return self.current_player[0]
+        return self.current_player
 
     def get_score(self):
         return self.current_matches
 
     def get_board(self):
-        random.shuffle(self.hidden_board)  # Shuffles words for new game
-        return self.hidden_board
-
-    def check_game_over(self):
-        if self.current_matches == self.TOTAL_MATCHES:
-            self.game_over = [True]
-
-    # Makes a turn, first_pos and second_pos are player's first and second guesses
-    def place_mark(self, first_pos, second_pos):
-        self.check_game_over()
-        first_pos = int(first_pos)
-        second_pos = int(second_pos)
-        self.blank_board[first_pos] = self.hidden_board[first_pos]
-        self.blank_board[second_pos] = self.hidden_board[second_pos]
-        self.check_same_word(first_pos, second_pos)
+        return self.blank_board
 
     def reset(self):
-        self.game_over = [False]
+        self.game_over = False
         self.current_matches = 0
-        self.current_player = []
-        self.hidden_board = ['Blimey!', 'Blimey!', 'Rubbish!', 'Rubbish!', 'Minging!' 'Minging!', 'Cheerio!', 'Cheerio!',
-                         'Codswallop! ', 'Codswallop!', 'Gutted', 'Gutted', 'Bobs Your Uncle!', 'Bobs Your Uncle!',
-                         'Brilliant!', 'Brilliant!']  # 16 spaces
-        self.blank_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.current_player = None
+        self.chosen_positions = []
+        self.original_board = ['Blimey!', 'Blimey!', 'Rubbish!', 'Rubbish!', 'Minging!', 'Minging!', 'Cheerio!',
+                               'Cheerio!',
+                               'Codswallop!', 'Codswallop!', 'Gutted', 'Gutted', 'Bobs Your Uncle!',
+                               'Bobs Your Uncle!',
+                               'Brilliant!', 'Brilliant!']  # 16 spaces
+        self.blank_board = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
         self.TOTAL_MATCHES = 8
+        self.hidden_board = random.sample(self.original_board, len(self.original_board))  # Shuffles words for new game
+        print(self.hidden_board)
