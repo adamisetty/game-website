8e94a876d094bac3da4ae50e1f0d2c653b2d73d2
ankithadamisetty <ankithad@illinois.edu>
2020-08-04 19:45:43 -0500

    Displays letter buttons without correct action

--- my-app/src/anagrams.js
+++ my-app/src/anagrams.js
@@ -6,6 +6,7 @@ const flaskApiUrl = "http://127.0.0.1:5000";
 
 class Letter extends React.Component {
     render() {
+        console.log(this.props.value);
         return (
           <button
             className="letter"
@@ -17,23 +18,34 @@ class Letter extends React.Component {
       }
 }
 
+class Submit extends React.Component {
+    render() {
+        return (
+            <button
+                className="submit"
+            >
+                {'submit'}
+            </button>
+        )
+    }
+}
+
 class AnagramsBoard extends React.Component {
 
     constructor(props){
         super(props);
         this.state = {
-            letters: Array(6).fill('_')
+            letters: Array(6).fill('_'),
+            word:''
         };
     this.myAPI = new API({url: flaskApiUrl});
     this.myAPI.createEntity({name: 'anagrams'});
-    //this.myAPI.endpoints.anagrams.create_game({game: 'anagrams'});
     this.fillLetters();
     }
 
     async fillLetters() {
         try {
             var start_data = await this.myAPI.endpoints.anagrams.create_game({game: 'anagrams'});
-            //console.log(start_data.data["games_data"][0]["board"])
             const letters = this.state.letters;
             for (let i = 0; i < 6; i++) {
                 var single_letter = start_data.data["games_data"][0]["board"][i];
@@ -46,10 +58,52 @@ class AnagramsBoard extends React.Component {
         }
     }
 
+    async handleLetterClick(i) {
+        const letters = this.state.letters;
+        this.state.letters[i] = 'X';
+        this.setState({letters: letters});
+    }
+
+    async handleSubmitClick() {}
+
+    renderTile(i) {
+        console.log(this.state.letters[i]);
+        return (
+            <Letter
+                value = {this.state.letters[i]}
+                onClick={() => this.handleLetterClick(i)}
+            />
+        );
+    }
+
+    renderSubmitButton() {
+        return (
+            <Submit
+                currentWord = {""}
+                onClick={() => this.handleSubmitClick()}
+            />
+        );
+    }
+
     render() { 
         return(
             <div>
-            <h1>HELLO</h1>
+                <div className="anagrams-title">
+                    <p> Anagrams </p>
+                </div>
+                <div className="row" style={{position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -10%)'}}>
+                {this.renderTile(0)}
+                {this.renderTile(1)}
+                {this.renderTile(2)}
+                {this.renderTile(3)}
+                {this.renderTile(4)}
+                {this.renderTile(5)}
+                </div>
+                <div className= "row" style={{position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, 100%)'}}>
+                <Submit></Submit>
+                </div>
         </div>
         )
     }
