6b2d56785494a229eef8b0924a3a5cfc0592ae5b
alyciab2 <alyciab2@illinois.edu>
2020-08-09 23:03:36 -0500

    has a working gameover for bullsandcows

--- my-app/src/bullsandcows.js
+++ my-app/src/bullsandcows.js
@@ -60,7 +60,10 @@ class BullsAndCowsBoard extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            guess:''
+            guess:'',
+            gameover: false,
+            secretcode: '',
+            score: ''
         };
     this.myAPI = new API({url: flaskApiUrl});
     this.myAPI.createEntity({name: 'bullsandcows'});
@@ -79,6 +82,23 @@ class BullsAndCowsBoard extends React.Component {
         return false;
     }
 
+    //the follwing method is from https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
+    formatTime(seconds) {
+        var hrs = ~~(seconds / 3600);
+        var mins = ~~((seconds % 3600) / 60);
+        var secs = ~~seconds % 60;
+
+        var time = "";
+
+        if (hrs > 0) {
+            time += "" + hrs + ":" + (mins < 10 ? "0" : "");
+        }
+
+        time += "" + mins + ":" + (secs < 10 ? "0" : "");
+        time += "" + secs;
+        return time;
+    }
+
     async handleNumberClick(num) { 
         var isDuplicate = this.isDuplicates(num);
         if ((this.state.guess.length < 4) && (!isDuplicate)) {
@@ -93,7 +113,15 @@ class BullsAndCowsBoard extends React.Component {
         var game_data = await this.myAPI.endpoints.bullsandcows.make_turn({game: 'bullsandcows'}, {position: guess});
         this.setState({guess:''});
         console.log(game_data.data["games_data"][0]["winner"]);
+        console.log(game_data.data["games_data"][0]["isWinner"]);
         console.log(game_data.data["games_data"][0]["board"]);
+        if (game_data.data["games_data"][0]["isWinner"] === true) {
+            var code = game_data.data["games_data"][0]["winner"];
+            var score = this.formatTime(parseInt(game_data.data["games_data"][0]["score"]));
+            this.setState({gameover: true});
+            this.setState({secretcode: code});
+            this.setState({score: score});
+        }
     }
 
     async handleClearClick() {
@@ -134,36 +162,53 @@ class BullsAndCowsBoard extends React.Component {
     }
 
     render() { 
-        return(
-            <div>
-                <div className="bullsandcows-title" style={{position: 'absolute', left: '50%', top: '0%',
-                    transform: 'translate(-50%, 0%)'}}>
-                    <p> Bulls and Cows </p>
+        if(this.state.gameover === false) {
+            return(
+                <div>
+                    <div className="bullsandcows-title" style={{position: 'absolute', left: '50%', top: '0%',
+                        transform: 'translate(-50%, 0%)'}}>
+                        <p> Bulls and Cows </p>
+                    </div>
+                    <div style={{position: 'absolute', left: '50%', top: '25%',
+                        transform: 'translate(-50%, 0%)'}}>
+                        {this.renderGuessArea()}
+                    </div>
+                    <div className="row" style={{position: 'absolute', left: '50%', top: '40%',
+                        transform: 'translate(-50%, 0%)'}}>
+                    {this.renderNumber("0")}
+                    {this.renderNumber("1")}
+                    {this.renderNumber("2")}
+                    {this.renderNumber("3")}
+                    {this.renderNumber("4")}
+                    {this.renderNumber("5")}
+                    {this.renderNumber("6")}
+                    {this.renderNumber("7")}
+                    {this.renderNumber("8")}
+                    {this.renderNumber("9")}
+                    </div>
+                    <div className= "row" style={{position: 'absolute', left: '50%', top: '50%',
+                        transform: 'translate(-50%, 10%)'}}>
+                    {this.renderSubmitButton()}
+                    {this.renderClearButton()}
+                    </div>
                 </div>
-                <div style={{position: 'absolute', left: '50%', top: '25%',
-                    transform: 'translate(-50%, 0%)'}}>
-                    {this.renderGuessArea()}
+            );
+        } else {
+            return(
+                <div>
+                    <div className="bullsandcows-title" style={{position: 'absolute', left: '50%', top: '0%',
+                        transform: 'translate(-50%, 0%)'}}>
+                        <p> Bulls and Cows </p>
+                    </div>
+                    <div className="bullsandcows-gameover" style={{position: 'absolute', left: '50%', top: '30%',
+                        transform: 'translate(-50%, 0%)'}}>
+                        <h3> You won! You cracked the code!</h3>
+                        <p> Time ~ {this.state.score} </p>
+                        <p> Secret Code ~ {this.state.secretcode} </p>
+                    </div>                  
                 </div>
-                <div className="row" style={{position: 'absolute', left: '50%', top: '40%',
-                    transform: 'translate(-50%, 0%)'}}>
-                {this.renderNumber("0")}
-                {this.renderNumber("1")}
-                {this.renderNumber("2")}
-                {this.renderNumber("3")}
-                {this.renderNumber("4")}
-                {this.renderNumber("5")}
-                {this.renderNumber("6")}
-                {this.renderNumber("7")}
-                {this.renderNumber("8")}
-                {this.renderNumber("9")}
-                </div>
-                <div className= "row" style={{position: 'absolute', left: '50%', top: '50%',
-                    transform: 'translate(-50%, 10%)'}}>
-                {this.renderSubmitButton()}
-                {this.renderClearButton()}
-                </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
